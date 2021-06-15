feb583e56a4a43d2f9da7eb8d569917f535af208
Denis <denZha@ya.ru>
2021-06-15 13:22:33 +0300

    исправление 2 практической

--- script.js
+++ script.js
@@ -24,21 +24,45 @@ const personalMovieDB = {
 // console.log(personalMovieDB);
 
 //Раздел 2: Практическая №2
-for(let i = 0; i <= personalMovieDB.count; i++)
-{
-    i = prompt("Один из последних просмотренных фильмов?","");
+// for(let i = 0; i <= personalMovieDB.count; i++)
+// {
+//     i = prompt("Один из последних просмотренных фильмов?","");
 
-    for(let j = 0; j <= personalMovieDB.count; j++)
+//     for(let j = 0; j <= personalMovieDB.count; j++)
+//     {
+//         personalMovieDB.movies[i] = j = prompt("На сколько оцените его?","");
+//         if(j == null || personalMovieDB.movies[i].length > 50)
+//         {
+//             j--;
+//             i--;
+//             alert("либо пустая строка, либо длинна превышает 50 символов");
+//         }
+//     }
+// }
+
+//исправление как по курсу
+for(let i = 0; i <= personalMovieDB.count; i++){
+    const 
+        a = prompt("Один из последних просмотренных фильмов?",""),
+        b = prompt("На сколько оцените его?","");
+
+    if(a == null || b == null || a == "" || b == "" || a.length > 50)
     {
-        personalMovieDB.movies[i] = j = prompt("На сколько оцените его?","");
-        if(j == null || personalMovieDB.movies[i].length > 50)
-        {
-            j--;
+        console.log("Error");
+        
+        if (i == 0) 
+            i = 0;
+        else 
             i--;
-            alert("либо пустая строка, либо длинна превышает 50 символов");
-        }
+    }    
+    else
+    {
+        personalMovieDB.movies[a] = b;
+        console.log("Done");
     }
 }
+
+console.log(personalMovieDB);
 
 if(personalMovieDB.count < 10)
     console.log("Просмотрено довольно мало фильмов");
